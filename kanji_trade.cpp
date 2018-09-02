41948286eb0bafbb932c9fa2e817dcee2f1624c6
aozorakyozai <aozorakyozai@gmail.com>
2018-09-02 18:29:04 +0900

    コメント作成

---
+++ kanji_trade.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> // べき乗
+#include <string> // C++　文字列
+#include <iostream> // 表示
+#include <vector> // 配列
+
+std::string kanji_num_trade(std::string moji); //説明は、下記sample参照　ポインタで返す予定
+std::string all_Replace(std::string std, std::string target_std, std::string change_std); //すべて置換
+
+int main() {
+	std::string sample = "たとえば、「百垓百一京百十二兆二百億二百一万二百二十二」の中の「百」は六通りの置換をする";//入力
+
+	printf("main_before -> %s\n", sample.c_str());
+
+	std::string num_moji = kanji_num_trade(sample); // 置換
+
+	printf("main_after -> %s\n", num_moji.c_str()); //出力　.c_str()は先頭アドレス
+
+	return 0;
+}    
+
+std::string kanji_num_trade(std::string moji) //cでも作る予定
+{
+	//char keta_moji[][ 6 ] = {"十","百","千"};
+	std::string keta_4_moji[] = {"","万","億","兆","京","垓"};
+	std::string keta_moji[] = {"十","百","千"};
+	std::string kanji_num[] = {"一","二","三","四","五","六","七","八","九",""};
+	
+	moji = moji + "・・・・加工前";
+	printf("kansu_before -> %s\n", moji.c_str());
+// ループ　置換内容の追加から
+	moji = all_Replace(moji, keta_moji[1], "ABC" + std::to_string(((10 % 4) + (int)(9 / 10)) * (int)pow(3,2)));
+
+	std::cout << "kansu_after -> " + moji + keta_moji[2] + "後" << std::endl;
+	// ketamojinosyori
+	
+	return moji + "後";
+}
+
+std::string all_Replace(std::string std1, std::string target_std, std::string change_std)
+{
+    std::string::size_type  Pos(std1.find(target_std));
+
+    while(Pos != std::string::npos)
+    {
+        std1.replace(Pos, target_std.length(), change_std);
+        Pos = std1.find(target_std, Pos + change_std.length());
+    }
+
+    return std1;
+}
