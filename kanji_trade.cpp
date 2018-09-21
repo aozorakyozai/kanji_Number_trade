ed847b256e11aab4926ea514911003d840addf68
aozorakyozai <aozorakyozai@gmail.com>
2018-09-21 21:50:08 +0900

    sample コメント作成

--- kanji_trade.cpp
+++ kanji_trade.cpp
@@ -3,9 +3,9 @@
 #include <string> // 文字列
 #include <iostream> // 表示
 
-std::string kanji_num_trade(std::string); //説明は、下記sample参照　ポインタで返す予定
-std::string all_Replace(std::string, std::string, std::string); //すべて置換
-std::string legal_doc(std::string); //　用途別　法律文書用
+std::string kanji_num_trade(std::string); // 漢数字を算用数字に置換
+std::string all_Replace(std::string, std::string, std::string); // すべて置換（sample参照）
+std::string legal_doc(std::string); // 用途別　法律文書用
 std::string irregular(std::string); // 例外処理
 
 int main() {
@@ -13,22 +13,28 @@ int main() {
 	//printf("漢数字を入力してください。\n");
 	//std::scanf("%s", &sample);
 
-	std::string sample = "Ex．「百垓百一京百十二兆二百億二百一万二百二十二」の中の「百」は六通りの置換がある。";//入力
+	std::string sample =  //入力
+    "変換範囲は、「一」から「九千九百九十九垓九千九百九十九京九千九百九十九兆九千九百九十九億九千九百九十九万九千九百九十九」です。"
+    "本プログラムは計算によらずに置換で行うため、最大値は、[unsigned long long]より大きい、無限大数(10の68乗+3)まで可能です。"
+    "問題点は、「百垓百一京百十二兆二百億二百一万二百二十二」の中の「百」は六通りの置換があること。"
+    "さらに、「十、百、千」と「万、億、兆、垓・・・」では変換内容が異なります。"
+    "また、一般文書の場合は、一石二鳥、十中八九、四十肩などの語句をもとに戻す処理を追加する必要があり四苦八苦します。"
+    "郵便番号、法律文書については、プログロム内をコメントアウトすることで、ほぼ正確に変換します。";
 	
-	printf("main_before -> %s\n", sample.c_str());
+	printf("【変換前】\n -> %s\n\n", sample.c_str());
 
 	std::string num_moji = kanji_num_trade(sample); // 置換
 
-	printf("main_after -> %s\n", num_moji.c_str()); //出力
+	printf("【変換後】\n -> %s\n", num_moji.c_str()); //出力
 
 	return 0;
 }    
 
 std::string kanji_num_trade(std::string moji) // 置換
 {
-	#define keta_moji_ARRAY_SIZE 3
-	#define KETA_4 4 //4ケタごとに出現する[keta_4_moji]から総桁数を求める
-	#define KETA_0 1 //[keta_4_moji]の要素番号の最大値を取得するため
+	#define keta_moji_ARRAY_SIZE 4 // {"十","百","千"}の3ケタ+1を追加する
+	#define KETA_4 4 // 4ケタごとに出現する[keta_4_moji]から総桁数を求める
+	#define KETA_0 1 // [keta_4_moji]の要素番号の最大値を取得するため
     #define nine_trade 2 // kanji_num 文字列中の九まで置換
 
 	std::string keta_4_moji[] = {"","万","億","兆","京","垓"};
@@ -109,19 +115,6 @@ std::string kanji_num_trade(std::string moji) // 置換
 	return moji;
 }
 
-std::string all_Replace(std::string std1, std::string target_std, std::string change_std)
-{
-    std::string::size_type  Pos(std1.find(target_std));
-
-    while(Pos != std::string::npos)
-    {
-        std1.replace(Pos, target_std.length(), change_std);
-        Pos = std1.find(target_std, Pos + change_std.length());
-    }
-
-    return std1;
-}
-
 std::string legal_doc(std::string std2) // 行政文書、法律文書用
 {
     std2 = all_Replace(std2, "1部", "一部");
@@ -145,11 +138,27 @@ std::string legal_doc(std::string std2) // 行政文書、法律文書用
 
 std::string irregular(std::string std3) //例外処理  (漢字でなければならない文字)
 {
-    std3 = all_Replace(std3, "十中八九", "十中八九");
-    std3 = all_Replace(std3, "十二単衣", "十二単衣");
-    std3 = all_Replace(std3, "五十嵐", "五十嵐");
-    std3 = all_Replace(std3, "四十肩", "四十肩");
+    std3 = all_Replace(std3, "10中八9", "十中八九");
+    std3 = all_Replace(std3, "12単衣", "十二単衣");
+    std3 = all_Replace(std3, "50嵐", "五十嵐");
+    std3 = all_Replace(std3, "40肩", "四十肩");
+    std3 = all_Replace(std3, "1石2鳥", "一石二鳥");
+    std3 = all_Replace(std3, "4苦8苦", "四苦八苦");
+
     //・・・・・・必要に応じて追加
 
     return std3;
+}
+
+std::string all_Replace(std::string std1, std::string target_std, std::string change_std)
+{
+    std::string::size_type  Pos(std1.find(target_std));
+
+    while(Pos != std::string::npos)
+    {
+        std1.replace(Pos, target_std.length(), change_std);
+        Pos = std1.find(target_std, Pos + change_std.length());
+    }
+
+    return std1;
 }
