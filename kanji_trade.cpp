5423e137d80b5a61afe8798de1b56534d3d679db
aozorakyozai <aozorakyozai@gmail.com>
2018-09-21 20:44:02 +0900

    例外処理追加

--- kanji_trade.cpp
+++ kanji_trade.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string> // C++　文字列
+#include <string> // 文字列
 #include <iostream> // 表示
 
-std::string kanji_num_trade(std::string moji); //説明は、下記sample参照　ポインタで返す予定
-std::string all_Replace(std::string std, std::string target_std, std::string change_std); //すべて置換
+std::string kanji_num_trade(std::string); //説明は、下記sample参照　ポインタで返す予定
+std::string all_Replace(std::string, std::string, std::string); //すべて置換
+std::string legal_doc(std::string); //　用途別　法律文書用
+std::string irregular(std::string); // 例外処理
 
 int main() {
 	//std::string sample;
@@ -22,16 +24,17 @@ int main() {
 	return 0;
 }    
 
-std::string kanji_num_trade(std::string moji) //cでも作る予定
+std::string kanji_num_trade(std::string moji) // 置換
 {
 	#define keta_moji_ARRAY_SIZE 3
 	#define KETA_4 4 //4ケタごとに出現する[keta_4_moji]から総桁数を求める
 	#define KETA_0 1 //[keta_4_moji]の要素番号の最大値を取得するため
+    #define nine_trade 2 // kanji_num 文字列中の九まで置換
 
 	std::string keta_4_moji[] = {"","万","億","兆","京","垓"};
 	std::string keta_moji[] = {"十","百","千"};
 	std::string kanji_num[] = {"一","二","三","四","五","六","七","八","九",""};
-    
+
 	// 配列結合関数を作成する
 	std::string target_moji[] = {"一","二","三","四","五","六","七","八","九","十","百","千","万","億","兆","京","垓"};
 
@@ -59,15 +62,49 @@ std::string kanji_num_trade(std::string moji) //cでも作る予定
 					before_trade = kanji_num[k] + keta_moji[(i % KETA_4) - 1] + ("digit_" + std::to_string(j) + "_assist");
 					after_trade = ("digit_" + std::to_string(i + 1) + "_assist") + std::to_string((int)((k + 1) / 10) + ((k + 1) % 10)) + std::string(i - j, '0');
 				}	
+
 				moji = all_Replace(moji, before_trade, after_trade);
 			}
 		}
 	}
-// 文字列の種類別の処理
-// 例外処理
-// 後処理
+    
+// 行政文書、法律文書なら下記２つともコメントアウト
+#if (0) // 単純置換　郵便番号用
+    for (int i = 0; i < (sizeof(kanji_num) / sizeof(kanji_num[0])); i++) {
+        moji = all_Replace(moji, kanji_num[i], std::to_string(i + 1));
+    }
+    moji = all_Replace(moji,  "○" , std::to_string(0));
+#endif
 
+#if (1) // 一般文書用　一文字なら置換しない
+    for (int i = 0; i < (sizeof(kanji_num) / sizeof(kanji_num[0])); i++) {
+        moji = all_Replace(moji, "digit_1_assist" + std::to_string(i + 1), kanji_num[i]);
+    }
 
+    for (int i = 0; i < (sizeof(keta_moji) / sizeof(keta_moji[0])); i++) {
+        moji = all_Replace(moji, ("digit_" + std::to_string(i + 2) + "_assist") + std::to_string(1) + std::string(i + 1, '0'), keta_moji[i]);
+    }
+
+    for (int i = 0; i < (sizeof(keta_4_moji) / sizeof(keta_4_moji[0])); i++) {
+        moji = all_Replace(moji, ("digit_" + std::to_string(i * 4 + 1) + "_assist") + std::string(i * 4 + 1, '0'), keta_4_moji[i]);
+    }
+#endif
+
+    // 50枚など算用数字の方が見やすい場合
+    std::string unit_moji[] = {"個","人","枚","回","通"};
+    for (int i = 0; i < (sizeof(unit_moji) / sizeof(unit_moji[0])); i++) {
+        for (int j = 0; j < (sizeof(kanji_num) / sizeof(kanji_num[0])) - nine_trade; j++) {
+            moji = all_Replace(moji, kanji_num[j] + unit_moji[i], std::to_string((j + 1)) + unit_moji[i]);            // 未完成
+        }
+    }
+
+    //  アシスト文字を消去する
+    for (int i = 0; i < ( (sizeof(keta_4_moji) / sizeof(keta_4_moji[0]) - KETA_0 ) * KETA_4 + keta_moji_ARRAY_SIZE) + 1; i++) {
+        moji = all_Replace(moji,  ("digit_" + std::to_string(i + 1) + "_assist") , "");
+    }
+
+    moji = irregular(moji); //例外処理
+    moji = legal_doc(moji); //法律文書用
 
 	return moji;
 }
@@ -83,4 +120,36 @@ std::string all_Replace(std::string std1, std::string target_std, std::string ch
     }
 
     return std1;
+}
+
+std::string legal_doc(std::string std2) // 行政文書、法律文書用
+{
+    std2 = all_Replace(std2, "1部", "一部");
+    std2 = all_Replace(std2, "1種", "一種");
+    std2 = all_Replace(std2, "1人", "一人");
+    std2 = all_Replace(std2, "1時", "一時");
+    std2 = all_Replace(std2, "1般", "一般");
+    std2 = all_Replace(std2, "1致", "一致");
+    std2 = all_Replace(std2, "1身", "一身");
+    std2 = all_Replace(std2, "1定", "一定");
+    std2 = all_Replace(std2, "1切", "一切");
+    std2 = all_Replace(std2, "同1", "同一");
+    std2 = all_Replace(std2, "1の地方", "一の地方");
+    std2 = all_Replace(std2, "10分", "十分");
+    std2 = all_Replace(std2, "2重", "二重");
+    std2 = all_Replace(std2, "第3者", "第三者");
+    std2 = all_Replace(std2, "第3取得者", "第三取得者");
+
+    return std2;
+}
+
+std::string irregular(std::string std3) //例外処理  (漢字でなければならない文字)
+{
+    std3 = all_Replace(std3, "十中八九", "十中八九");
+    std3 = all_Replace(std3, "十二単衣", "十二単衣");
+    std3 = all_Replace(std3, "五十嵐", "五十嵐");
+    std3 = all_Replace(std3, "四十肩", "四十肩");
+    //・・・・・・必要に応じて追加
+
+    return std3;
 }
